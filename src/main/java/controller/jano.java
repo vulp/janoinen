279c00ff3bf990cf5d5b359782c18480f4f423e8
vulp <tuomo.pohjola@gmail.com>
2013-01-06 18:29:43 +0200

    asdf

--- src/main/java/controller/jano.java
+++ src/main/java/controller/jano.java
@@ -8,6 +8,19 @@ package controller;
  * To change this template use File | Settings | File Templates.
  */
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
+@RequestMapping("/jano")
 public class jano {
+    @RequestMapping()
+	public String printWelcome(Model model) {
+
+        //todo hae parse metodista tulos tähän ja palauta se modelissa
+
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "hello";
+
+	}
 }

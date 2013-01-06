e33347a95d8682488e3e5ada1e682753ae9199b3
vulp <tuomo.pohjola@gmail.com>
2013-01-06 18:22:52 +0200

    update

---
+++ src/main/java/controller/jano.java
@@ -0,0 +1,13 @@
+package controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vulp
+ * Date: 1/6/13
+ * Time: 6:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import org.springframework.stereotype.Controller;
+@Controller
+public class jano {
+}

22cc4cd51c723941686197b8a599765fecd141f0
vulp <tuomo.pohjola@gmail.com>
2013-01-10 20:08:34 +0200

    pari parser luokkaa lisää

---
+++ src/main/java/parsers/Joutsen.java
@@ -0,0 +1,11 @@
+package parsers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vulp
+ * Date: 1/10/13
+ * Time: 8:06 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Joutsen {
+}
---
+++ src/main/java/parsers/Kitty.java
@@ -0,0 +1,12 @@
+package parsers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vulp
+ * Date: 1/10/13
+ * Time: 8:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Kitty {
+    //todo copy paste kaisla
+}

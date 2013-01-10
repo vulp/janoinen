1aa0711248c19eb86cdae1422215c7eaf5499d97
vulp <tuomo.pohjola@gmail.com>
2013-01-10 20:17:47 +0200

    pari raflaa

---
+++ src/main/java/parsers/Hostina.java
@@ -0,0 +1,11 @@
+package parsers;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vulp
+ * Date: 1/10/13
+ * Time: 8:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Hostina {
+}

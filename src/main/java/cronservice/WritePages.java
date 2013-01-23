1a11dc475352aaf60d8966bf76f022df5cbff13c
vulp <tuomo.pohjola@gmail.com>
2013-01-23 16:29:38 +0200

    cron service for webpage parsing started

---
+++ src/main/java/cronservice/WritePages.java
@@ -0,0 +1,19 @@
+package cronservice;
+
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vulp
+ * Date: 1/23/13
+ * Time: 4:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class WritePages {
+
+    //@Scheduled(cron="*/1 * * * * MON")
+    public void getPages(){
+
+    }
+}
